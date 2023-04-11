33eb55742b512f26ca8d6880c4bb2786630889c7
Sawat Lapprasertlum <88827707+SawatKia@users.noreply.github.com>
2023-04-11 13:52:43 +0700

    DB's table config

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,15 @@
+﻿using bascicASP.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace bascicASP.Data
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) :base(options){ 
+
+        }
+        //model Student create a database table name Students by reference the column from Student model properties and store in StudentDB
+        //represent Students table in DB
+        public DbSet<Student> Students { get; set; } 
+    }
+}
