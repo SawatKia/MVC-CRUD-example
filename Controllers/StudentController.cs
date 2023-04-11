a2fe7c04214183a773ded0441c7e22094f1c704c
Sawat Lapprasertlum <88827707+SawatKia@users.noreply.github.com>
2023-04-11 13:53:24 +0700

    MVC

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,91 @@
+﻿using bascicASP.Data;
+using bascicASP.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bascicASP.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+
+        //dependency injection make you can access to table in db via object(from ApplicationDBContext) _db
+        //constructure
+        public StudentController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable <Student> allStudent = _db.Students;
+            return View(allStudent);  
+        }
+        //default is GET method
+        public IActionResult Create() {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]// check no script in input
+        public IActionResult Create(Student obj)
+        {
+            //check input requirement from data-annontation in model Student
+            //such as [required],[range(0,100)]
+            if(ModelState.IsValid)
+            {
+                _db.Students.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);//return data the have informed
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Students.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]// check no script in input
+        public IActionResult Edit(Student obj)
+        {
+            //check input requirement from data-annontation in model Student
+            //such as [required],[range(0,100)]
+            if (ModelState.IsValid)
+            {
+                _db.Students.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);//return data the have informed
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            //ค้นหาข้อมูล
+            var obj = _db.Students.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Students.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+
+}
---
+++ Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace bascicASP.Models
+{
+    public class Student
+    {
+        [Key]//make Id is a primary key
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="กรุณาป้อนชื่อนักศึกษา")]
+        [DisplayName("ชื่อนักศึกษา")]
+        public string Name { get; set; }
+
+        [DisplayName("คะแนนสอบ")]
+        [Range(0,100, ErrorMessage ="กรุณาป้อนตัวเลขในช่วง 0-100")]
+        public int Score { get; set; }
+    }
+}
